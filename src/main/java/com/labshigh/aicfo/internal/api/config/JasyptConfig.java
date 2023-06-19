a6a1baf813c22cd80947199e87422136776a02cb
nft7274 <nft7274@nftmoa.io>
2023-06-19 17:13:31 +0900

    [AICFO-1]  Jasypt props 경로 변경

--- src/main/java/com/labshigh/aicfo/internal/api/config/JasyptConfig.java
+++ src/main/java/com/labshigh/aicfo/internal/api/config/JasyptConfig.java
@@ -2,18 +2,15 @@ package com.labshigh.aicfo.internal.api.config;
 
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import lombok.extern.log4j.Log4j2;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @EnableEncryptableProperties
@@ -28,6 +25,8 @@ public class JasyptConfig {
   private String stringOutputType;
   @Value("${jasypt.encryptor.key-obtention-iterations}")
   private int keyObtentionIterations;
+  @Value("${jasypt.props.file-path}")
+  private String propsPath;
 
   @Bean
   public StringEncryptor jasyptStringEncryptor() {
@@ -42,16 +41,19 @@ public class JasyptConfig {
   }
 
   private String getJasyptPassword() {
-    ClassPathResource resource = new ClassPathResource("jasypt.props");
+
     String result = null;
     try {
-      result = new BufferedReader(new InputStreamReader(resource.getInputStream(),
-          StandardCharsets.UTF_8)).lines().collect(Collectors.joining(""));
 
-      return result;
+      result = String.join("", Files.readAllLines(Paths.get(propsPath)));
+
+//      result = new BufferedReader(new InputStreamReader(resource.getInputStream(),
+//          StandardCharsets.UTF_8)).lines().collect(Collectors.joining(""));
+
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+    return result;
   }
 
 }
